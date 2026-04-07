663ccb0c31130a8ad1ddc9c4faa7593d012d78ee
Liubov Shilova <57145211+LiubovShilova@users.noreply.github.com>
2026-04-07 09:24:50 +0200

    Refactor image processing function
    Updated the process function to include the img_size parameter and crop the image if it is not square.

--- EyeQ_preprocess/EyeQ_process_main.py
+++ EyeQ_preprocess/EyeQ_process_main.py
@@ -1,11 +1,13 @@
-import fundus_prep as prep
+import sys
+sys.path.append('./')
+from . import fundus_prep as prep
 import glob
 import os
 import cv2 as cv
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-def process(image_list, save_path):
+def process(image_list, save_path, img_size = (800,800)):
     
     for image_path in image_list:
         dst_image = os.path.splitext(image_path.split('/')[-1])[0]+'.png'
@@ -15,15 +17,15 @@ def process(image_list, save_path):
             continue
         try:
             img = prep.imread(image_path)
-            r_img, borders, mask = prep.process_without_gb(img)
+            #adjusted for cropping instead of black padding
+            r_img, borders, mask = prep.process_without_gb_with_center_crop(img)
             r_img = cv.resize(r_img, (800, 800))
             prep.imwrite(dst_path, r_img)
             # mask = cv.resize(mask, (800, 800))
             # prep.imwrite(os.path.join('./original_mask', dst_image), mask)
-        except:
-            print(image_path)
+        except Exception as e:
+            print(f"Error processing {image_path}: {e}")
             continue
-
 
 if __name__ == "__main__":
 
